a6c7e93efd6f52319251c7bfa678a4ef9de556aa
Nikhil <nikhil.gupta@cipl.org.in>
2025-01-31 14:47:02 +0530

    added the navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LeftSideBar from './components/leftSideBar/leftSideBar';
-import "./App.css"
-import NavBar from './components/navBar/navBar';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LeftSideBar from "./components/leftSideBar/leftSideBar";
+import NavBar from "./components/navBar/navBar";
+import "./App.css";
 
 function App() {
   return (
-    <div className='body' style={{display:"flex"}}>
-      <LeftSideBar/>
-      <div>
-        {/* <NavBar/> */}
+    <Router>
+      <div className="body">
+        {/* Left Sidebar */}
+        <LeftSideBar />
+
+        {/* Main Content Area */}
+        <div className="main-content">
+          {/* Navbar */}
+          <NavBar />
+
+          {/* Routes for different pages */}
+          <Routes>
+            <Route path="/" element={<h1>Dashboard</h1>} />
+            <Route path="/reports" element={<h1>Reports</h1>} />
+            <Route path="/billing" element={<h1>Billing Estimates</h1>} />
+            {/* Add more routes here */}
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -3,24 +3,31 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { FaRegBell } from "react-icons/fa";
 import { SiSunrise } from "react-icons/si";
 import { RxAvatar } from "react-icons/rx";
+import './NavBar.css';  // Import the CSS file
 
-function NavBar(){
-
-    return(
-        <div style={{border:"1px solid black",backgroundColor:"white",display:"flex",alignItems:"center",justifyContent:"space-between",width:"1020px"}}>
-            <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-                <GiHamburgerMenu/>
-                <p>Consumption Intelligence</p>
-            </div>
-            <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
-            <FaRegBell/>
-            <SiSunrise/>
-            <p>Nishant Garg</p>
-            <RxAvatar/>
-            </div>
+function NavBar() {
+  return (
+    <div className="navbar">
+      {/* Left side - Hamburger menu and title */}
+      <div className="navbar-left">
+        <div className="hamburger-icon">
+          <GiHamburgerMenu className="hamburger-menu" />
         </div>
-    )
-    
+        <p className="navbar-title">Consumption Intelligence</p>
+      </div>
+
+      {/* Right side - Icons and Avatar */}
+      <div className="navbar-right">
+        <FaRegBell className="navbar-icon" />
+        <div className="vertical-line"></div>
+        <SiSunrise className="navbar-icon" />
+        <div className="vertical-line"></div>
+        <p className="navbar-text">Nishant Garg</p>
+        <div className="vertical-line"></div>
+        <RxAvatar className="navbar-icon" />
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;

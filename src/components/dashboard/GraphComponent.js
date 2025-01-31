852defadfd84accac42bb0a853176486c23ee980
Nikhil <nikhil.gupta@cipl.org.in>
2025-01-31 16:18:44 +0530

    added the grpah

--- src/components/dashboard/GraphComponent.js
+++ src/components/dashboard/GraphComponent.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Pie, Bar, Line } from 'react-chartjs-2';
 import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale, BarElement, LineElement, PointElement } from 'chart.js';
 
+// Import the CSS file for Graph component styles
+import './GraphComponent.css';
+
 // Register Chart.js components
 ChartJS.register(
   Title,
@@ -17,16 +20,38 @@ ChartJS.register(
 
 const GraphComponent = ({ title, chartType, data }) => {
   let chart;
-  
+
   switch (chartType) {
     case 'pie':
-      chart = <Pie data={data} />;
+      chart = (
+        <div className="pie-chart-container">
+          <Pie data={data} />
+        </div>
+      );
       break;
     case 'bar':
-      chart = <Bar data={data} />;
+      const barData = {
+        ...data,
+        datasets: data.datasets.map((dataset) => ({
+          ...dataset,
+          backgroundColor: '#BFE3F8', // Light blue color for the bars
+          borderColor: '#2461C4', // Color for the border of bars
+          barThickness: 12, // Make bars smaller
+        })),
+      };
+
+      chart = (
+        <div className="bar-chart-container">
+          <Bar data={barData} />
+        </div>
+      );
       break;
     case 'line':
-      chart = <Line data={data} />;
+      chart = (
+        <div className="line-chart-container">
+          <Line data={data} />
+        </div>
+      );
       break;
     default:
       chart = null;

dbe1ededd92fb1c1acb6ea860f4dd03f990f9f21
Nikhil Gupta <nikhilgupta@Nikhils-MacBook-Air.local>
2025-02-01 10:54:51 +0530

    remove bootstap from index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store/store';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

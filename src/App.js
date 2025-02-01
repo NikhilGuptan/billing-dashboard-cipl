6ed766269e1f61153e71b51f034447d7c4c73e2c
Nikhil Gupta <nikhilgupta@Nikhils-MacBook-Air.local>
2025-02-01 09:19:10 +0530

    added footer as well

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LeftSideBar from "./components/leftSideBar/leftSideBar";
 import NavBar from "./components/navBar/navBar";
+import Footer from "./components/footer/Footer";
 import "./App.css";
 import DashboardComponent from "./components/dashboard/dashboardComponent";
 
@@ -14,11 +15,8 @@ function App() {
           <NavBar />
           <Routes>
             <Route path="/" element={<DashboardComponent />} />
-            {/* <Route path="/reports" element={<ReportsComponent />} />
-            <Route path="/billing" element={<BillingComponent />} />
-            <Route path="/projected-charges" element={<ProjectedChargesComponent />} />
-            <Route path="/capacity" element={<CapacityComponent />} /> */}
           </Routes>
+          <Footer /> {/* Add Footer here */}
         </div>
       </div>
     </Router>
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Design and Developed By Corporate Infotech Pvt. Ltd.</p>
+      <p>© All Rights Reserved 2025</p>
+    </div>
+  );
+}
+
+export default Footer;

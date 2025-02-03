d9c046978947df1747cc337b2612e988c7b28b1a
Nikhil <nikhil.gupta@cipl.org.in>
2025-02-03 14:33:11 +0530

    added mui for grpah chart

--- src/components/dashboard/GraphComponent.js
+++ src/components/dashboard/GraphComponent.js
@@ -1,39 +1,92 @@
-import { Bar, Line, Pie } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement, ArcElement } from "chart.js";
-import "./DashboardComponent.css";
-
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement, ArcElement);
+import React from "react";
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { LineChart } from '@mui/x-charts/LineChart';
 
 const GraphComponent = ({ title, data, type }) => {
-  const chartTypes = { bar: Bar, line: Line, pie: Pie };
-  const ChartTag = chartTypes[type] || Bar;
-
   const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: false,
-      tooltip: { enabled: true },
-    },
+    height: 300,
+    margin: { top: 20, right: 20, bottom: 20, left: 20 },
   };
 
   if (type === "pie") {
-    options.plugins.datalabels = {
-      formatter: (value, context) => `${value}%`,
-      color: "#000",
-      font: { weight: "bold" },
-    };
+    // Convert dataset to include percentage labels
+    const total = data.datasets[0].data.reduce((acc, val) => acc + val, 0);
+    const pieData = data.labels.map((label, index) => ({
+      id: index,
+      value: data.datasets[0].data[index],
+      percentage: ((data.datasets[0].data[index] / total) * 100).toFixed(1) + "%",
+    }));
+
+    // Define lighter colors
+    const lightColors = ["#FFCCBC", "#B3E5FC", "#C8E6C9", "#80DEEA"];
+
+    return (
+      <div className="graph-box">
+        <div className="graph-header">{title}</div>
+        <div className="graph-content">
+          <PieChart
+            series={[
+              {
+                data: pieData,
+                arcLabel: (item) => item.percentage, // Show percentage only
+                innerRadius: 10,
+                outerRadius: 120,
+                paddingAngle: 0,
+                colors: lightColors, // Apply light colors
+              },
+            ]}
+            sx={{
+              [`& .${pieArcLabelClasses.root}`]: {
+                fill: "#000",
+                fontSize: 14,
+                fontWeight: "bold",
+              },
+            }}
+            {...options}
+          />
+        </div>
+      </div>
+    );
   }
 
-  return (
-    <div className="graph-box">
-      <div className="graph-header">{title}</div>
-      <div className="graph-content">
-        <ChartTag data={data} options={options} />
+  if (type === "bar") {
+    return (
+      <div className="graph-box">
+        <div className="graph-header">{title}</div>
+        <div className="graph-content">
+          <BarChart
+            xAxis={[{ scaleType: "band", data: data.labels }]}
+            series={[{ data: data.datasets[0].data, color: data.datasets[0].backgroundColor }]}
+            {...options}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
 
+  if (type === "line") {
+    return (
+      <div className="graph-box">
+        <div className="graph-header">{title}</div>
+        <div className="graph-content">
+          <LineChart
+            xAxis={[{ scaleType: "point", data: data.labels }]}
+            series={[
+              {
+                data: data.datasets[0].data,
+                color: data.datasets[0].borderColor,
+                area: true, // Enables the area fill effect
+              },
+            ]}
+            {...options}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 export default GraphComponent;
